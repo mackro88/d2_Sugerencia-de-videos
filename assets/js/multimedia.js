461e471c1bc4904b074e552a4a631a8728a9a6d1
Romina Pineda <romi.macka@gmail.com>
2021-08-08 19:37:19 -0400

    terminado

--- assets/js/multimedia.js
+++ assets/js/multimedia.js
@@ -1,10 +1,14 @@
 "use strict";
 export class Multimedia{
+    #url;
     constructor(url){
         this.#url = url
     }
     get url(){
         return this.#url
+    }
+    set url(startTime){
+        this.#url = startTime
     }
     setStart(){
         console.log("Este método es para realizar un cambio en la URL del video")
@@ -17,9 +21,10 @@ export class Player extends Multimedia{
     }
     playMultimedia(){
         const category = document.getElementById(this.id)
+        console.log(category)
         category.setAttribute("src", this.url)
     }
     setStart(time){
-        super.#url += `?start=${time}`
+        super.url += `?start=${time}`
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,22 @@
 "use strict";
-
-//import Player from "./multimedia.js";
-
-class Multimedia{
-    constructor(url){
-        this.url = url
-    }
-    get url(){
-        return this.url
-    }
-    setStart(){
-        console.log("Este método es para realizar un cambio en la URL del video")
-    }
-}
-class Player extends Multimedia{
-    constructor(url,id){
-        super(url)
-        this.id = id
-    }
-    playMultimedia(){
-        const category = document.getElementById(this.id)
-        category.setAttribute("src", this.url)
-    }
-    setStart(time){
-        super.url += `?start=${time}`
-    }
-}
+import {Player} from './multimedia.js';
 
 const musica = new Player("https://www.youtube.com/embed/CfbTPqhxURY","musica")
 const peliculas = new Player("https://www.youtube.com/embed/eDaNejmf-1A","peliculas")
 const series = new Player("https://www.youtube.com/embed/7CeeroWynyI","series")
 
+musica.setStart(50)
 
+console.log(musica.url)
+console.log(peliculas.url)
+console.log(series.url)
 
-document.getElementById('collapseOne').addEventListener("click", () => {
+document.getElementById('cat_musica').addEventListener('click', () => {
     musica.playMultimedia()
 })
-document.getElementById('collapseTwo').addEventListener("click", () => {
+document.getElementById('cat_peliculas').addEventListener('click', () => {
     peliculas.playMultimedia()
 })
-document.getElementById('collapseThree').addEventListener("click", () => {
+document.getElementById('cat_series').addEventListener('click', () => {
     series.playMultimedia()
-})
+}) 
